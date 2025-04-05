a9a5f17b0d7d5d601eb37a3c7e963c3f5961166f
Loc Do <locdoduc2k1@gmail.com>
2025-04-05 11:00:15 +0700

    initialize Vue 3 project with Vite, Tailwind CSS, and Flowbite components

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+input::-ms-reveal,
+input::-ms-clear {
+  display: none;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+import Login from '../pages/user/Login.vue'
+import Signup from '../pages/user/Signup.vue'
+
+const components = { Login, Signup }
+</script>
+
+<template>
+  <Login />
+  <Signup />
+</template>
