d129bf41de5975934d71abd20412b04567579bf1
Loc Do <locdoduc2k1@gmail.com>
2025-06-01 07:30:53 +0700

    Add form elements to source code

--- src/components/admin/layout/Backdrop.vue
+++ src/components/admin/layout/Backdrop.vue
@@ -1,9 +1,5 @@
 <template>
-  <div
-    v-if="isMobileOpen"
-    class="fixed inset-0 bg-gray-900/50 z-9999 lg:hidden"
-    @click="toggleMobileSidebar"
-  ></div>
+  <div v-if="isMobileOpen" class="fixed inset-0 bg-gray-900/50 z-9999 lg:hidden" @click="toggleMobileSidebar"></div>
 </template>
 
 <script setup lang="ts">
